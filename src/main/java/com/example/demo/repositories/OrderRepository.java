c7c561d807d975af41089275691bc99710f0d467
Piotr Celowski <piotr.celowski@gmail.com>
2022-11-27 08:32:57 +0100

    Spring data jdbc

--- src/main/java/com/example/demo/repositories/OrderRepository.java
+++ src/main/java/com/example/demo/repositories/OrderRepository.java
@@ -1,7 +1,8 @@
 package com.example.demo.repositories;
 
 import com.example.demo.domain.TacoOrder;
+import org.springframework.data.repository.CrudRepository;
 
-public interface OrderRepository {
+public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
     TacoOrder save(TacoOrder order);
 }
